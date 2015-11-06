cbcccff82c9aa08f54677fd49450169e97c2a56c
Steven Popkin <sjpopkin@gmail.com>
2015-11-06 01:53:05 -0500

    Modified the makefile, created the asm file.

---
+++ x86/dice_roller.asm
@@ -0,0 +1,14 @@
+;-------------------------------------------------------------------------------
+;	file: dice_roller.asm
+;
+;	author: spopkin
+;
+;	Prints random numbers for each dice roll, as well as the total sum of
+;	the dice rolls.
+;
+;	build:
+;	nasm -f elf -o dice_roller dice_roller.asm
+;-------------------------------------------------------------------------------
+
+
+
